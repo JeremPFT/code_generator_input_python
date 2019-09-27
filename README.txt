fc95d018f5c2b3fa6d09ba6db81bd3512677478a
jpiffret <Jeremy.PIFFRET@ingenico.com>
2019-09-27 17:29:18 +0200

    WIP

---
+++ README.txt
@@ -0,0 +1,5 @@
+tutos:
+https://www.dabeaz.com/ply/ply.html
+https://developer.ibm.com/tutorials/au-lexyacc/
+http://www.cs.man.ac.uk/~pjj/cs2111/ex5_hint.html
+https://riptutorial.com/fr/python/example/31585/partie-2--analyse-d-entrees-tokenized-avec-yacc
---
+++ project_parser_lexer.py
@@ -0,0 +1,66 @@
+import ply.lex as lex
+
+reserved = {
+    'project'          : 'PROJECT_START',
+    'end_project'      : 'PROJECT_STOP',
+    'output_directory' : 'OUTPUT_DIRECTORY',
+    'package'          : 'PACKAGE_START',
+    'end_package'      : 'PACKAGE_STOP',
+    'class'            : 'CLASS_START',
+    '!end_class'       : 'CLASS_STOP',
+    'field'            : 'FIELD_START',
+    'end_field'        : 'FIELD_STOP',
+    'use'              : 'USE',
+}
+
+tokens = [
+    'IDENTIFIER',
+    'COMMENT',
+    'STRING',
+] + list(reserved.values())
+
+t_ignore  = ' \t'
+
+def t_IDENTIFIER(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'IDENTIFIER')
+    return t
+
+def t_COMMENT(t):
+    r'-- .*'
+    return t
+
+def t_STRING(t):
+    r'".*"'
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+lexer = lex.lex()
+
+
+data = '''
+project project_name
+output_directory "c:\"
+-- a basic example to test the parser
+end_project
+'''
+
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
+    print('column: ' + str(find_column(data, tok)))
