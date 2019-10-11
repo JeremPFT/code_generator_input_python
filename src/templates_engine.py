13ceee35c18aeaf959c24490946e539d390a3dc2
JeremPFT <j.piffret@gmail.com>
2019-10-11 03:51:11 +0200

    Iniitalize a customed template enginte
    the template files should be compatibles with ada library templates_parser

---
+++ src/templates_engine.py
@@ -0,0 +1,13 @@
+class Template_Engine():
+    VARIABLE_START = "@_"
+    VARIABLE_STOP = "_@"
+    # to get a behavior like ada library template_parser
+
+    def __init__(self):
+        pass
+
+    def set_template_directory(self, path):
+        pass
+
+    def render_template(self, file_name):
+        pass
