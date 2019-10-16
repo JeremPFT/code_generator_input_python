ea73430bbf8abeb9db39cb6dfbf7c69e61bef93e
JeremPFT <j.piffret@gmail.com>
2019-10-16 02:09:29 +0200

    Tests WIP

--- src/generator_ada_project.py
+++ src/generator_ada_project.py
@@ -8,32 +8,35 @@ from src.template_engine import Template_Engine
 class Generator_Ada_Project():
     def __init__(self, template_engine):
         if template_engine.__class__ != Template_Engine:
-            raise ValueError("template_engine must be of Template_Engine class")
+            raise TypeError("template_engine must be of Template_Engine class")
         self.__template_engine = template_engine
+        self.__project = None
 
     def output(self, project):
-        self._output_project(project)
-
-    def _output_project(self, project):
-        if project.__class__.__name__ != "Project":
+        if project.__class__ != Project:
             raise TypeError("Project instance expected")
 
-        prj_dir = os.path.normpath(project.output_directory())
+        self.__project = project
+        self.__output_project()
 
+    def __create_tree(self):
+        prj_dir = os.path.normpath(self.__project.output_directory())
         build_dir(prj_dir)
-        os.chdir(prj_dir)
-        build_dir("src")
+        build_dir(os.path.join(prj_dir, "src"))
 
-        project_name = capitalize_identifier(project.name)
+    def __output_project(self):
+        self.__create_tree()
 
-        print("project name: %s" % (project_name))
+        cap_project_name = capitalize_identifier(self.__project.name)
+
+        print("project name: %s" % (cap_project_name))
 
         te = self.__template_engine
 
         rendered_template = te.render (file_name    = "project.gpr",
-                                       dico = {'project_name' : project_name})
+                                       dico = {'project_name' : cap_project_name})
 
-        f = open(project.name + ".gpr", "w")
+        f = open(self.__project.name + ".gpr", "w")
         f.write(rendered_template)
         f.close()
 
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,3 +1,5 @@
+import os
+
 from src.generator_ada_project import Generator_Ada_Project
 from src.dsl_grammar import parser
 from src.template_engine import Template_Engine
@@ -10,3 +12,16 @@ print(str(project))
 template_engine = Template_Engine(template_directory = "tests/test_1")
 generator = Generator_Ada_Project(template_engine)
 generator.output(project)
+
+HOME = "c:/Users/jeremy/AppData/Roaming/"
+
+prj_dir = os.path.join(HOME, "tests/simple_project_1")
+src_dir = os.path.join(HOME, "tests/simple_project_1", "src")
+
+def check_dir(path, name):
+    if not os.path.exists(path):
+        print("ERROR: %s doesn't exist : %s" % (name, path))
+
+
+check_dir(prj_dir, "prj_dir")
+check_dir(src_dir, "src_dir")
